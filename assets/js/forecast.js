db72f83bfc8c6974ede65f26a4ce6d31f70e58b9
GenaPlem <shainkill1337@gmail.com>
2023-09-06 14:14:01 +0100

    Add function to render forecast to the forecast page

--- assets/js/forecast.js
+++ assets/js/forecast.js
@@ -10,7 +10,7 @@ const getForecast = () => {
                 console.log(res)
                 const {location:{name}, forecast:{forecastday}} = res
 
-
+                mainContent.innerHTML = renderForecast(name, forecastday)
                 // error.style.display = 'none'
             })
     } catch (error) {
@@ -22,3 +22,34 @@ const getForecast = () => {
 document.addEventListener('DOMContentLoaded', () => {
     getForecast()
 })
+
+const renderForecast = (name, forecastday) => {
+    return `<div class="weather glassmorphism">
+                    <h2 class="hidden_heading">Forecast weather</h2>
+                    <h3 class="forecast__name">${name}</h3>
+                    <div class="forecast">
+                        <div class="forecast__day glassmorphism">
+                            <img class="forecast__icon" src="assets/images/weather_clouds_black.svg" alt="">
+                            <span class="forecast__date">${forecastday[0].date}</span>
+                            <p class="forecast__weather">${forecastday[0].day.condition.text}</p>
+                            <span class="forecast__temp">H: ${forecastday[0].day.maxtemp_c}℃  L: ${forecastday[0].day.mintemp_c}℃</span>
+                        </div>
+                    </div>
+                    <div class="forecast">
+                        <div class="forecast__day glassmorphism">
+                            <img class="forecast__icon" src="assets/images/weather_clouds_black.svg" alt="">
+                            <span class="forecast__date">${forecastday[1].date}</span>
+                            <p class="forecast__weather">${forecastday[1].day.condition.text}</p>
+                            <span class="forecast__temp">H: ${forecastday[1].day.maxtemp_c}℃  L: ${forecastday[1].day.mintemp_c}℃</span>
+                        </div>
+                    </div>
+                    <div class="forecast">
+                        <div class="forecast__day glassmorphism">
+                            <img class="forecast__icon" src="assets/images/weather_clouds_black.svg" alt="">
+                            <span class="forecast__date">${forecastday[2].date}</span>
+                            <p class="forecast__weather">${forecastday[2].day.condition.text}</p>
+                            <span class="forecast__temp">H: ${forecastday[2].day.maxtemp_c}℃  L: ${forecastday[2].day.mintemp_c}℃</span>
+                        </div>
+                    </div>
+            </div>`
+}
